18b920dd3c4a7fa88abb3b834904a57fb3c4cf80
326804 <326804@studenti.unimore.it>
2024-02-14 12:21:11 +0100

    wifiFunzionante

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,46 @@
 #include <Arduino.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <PubSubClient.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <DHT.h>
+#include <Adafruit_Sensor.h>
+//#include <string.h>
+
+///////////////////////////////////////////////////////////
+const char* nomeRete = "FASTWEB-1-A91631_EXT";
+const char* pwdRete = "4452CC2BAB";
+///////////////////////////////////////////////////////////
+IPAddress ip_scheda(192,168,1,106);
+IPAddress ip_server(192,168,1,86);
+///////////////////////////////////////////////////////////
+WiFiClient wifiClient;
+PubSubClient client(ip_server,1883,wifiClient);
+///////////////////////////////////////////////////////////
+
+void connessioneaWiFi(){
+  Serial.println("Ingresso in CONNESSIONE_WIFI");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(nomeRete,pwdRete);
+  Serial.println("\nTentativo di connessione alla rete");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+  }
+  Serial.println("\nConnessione Stabilita con IP : ");
+  Serial.println(WiFi.localIP());
+}
 
 // put function declarations here:
-int myFunction(int, int);
+//int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //int result = myFunction(2, 3);
   Serial.begin(9600);
   Serial.println("Ingresso in SETUP");
+  connessioneaWiFi();
 }
 
 void loop() {
@@ -18,6 +51,6 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+//int myFunction(int x, int y) {
+//  return x + y;
+//}
